2bb59da1993e5fdd75919bb69aedd65dbb10101d
23080ad <23080ad@hvhs.school.nz>
2025-05-12 21:55:28 +0000

    rent car next time

---
+++ homePage.js
@@ -0,0 +1,21 @@
+//Title
+
+//Variables
+
+
+/*****************************
+Main code
+*****************************/
+console.log("Running homePage.css");
+
+
+/***************************** 
+Functions 
+*****************************/
+function askViewerName(){
+    viewerName = prompt("What is your name friend?")
+}
+
+function rentACar(){
+    viewerName = askViewerName ();
+}
